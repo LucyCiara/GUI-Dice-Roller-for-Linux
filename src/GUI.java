84950192f5e7c2140baf0c66fee060e4f5150bf5
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2025-07-10 18:54:51 +0200

    Functional window
    Created a little window

---
+++ src/GUI.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class GUI {
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0,1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Dice Roller");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int diceNumber = 0;
+        int diceType = 0;
+
+        new GUI();
+    }
+}
